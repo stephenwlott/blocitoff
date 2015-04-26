5fcb629ab8e99c6e41124f3f253bea7bf9fc4234
Stephen Lott <stephenwlott@gmail.com>
2015-04-26 06:21:59 +0000

    added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
+  
   get 'welcome/home'
 
   get 'welcome/about'
 
+  authenticated :user do
+    root to: "users#show", :as => "profile"
+  end
   root to: 'welcome#home'
 end
